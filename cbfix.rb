0d4d30b24e374d30e2395ec1aa261c7ab67f3874
Konstantin Lysenko <gshaud@gmail.com>
2013-12-25 23:53:54 +0700

    Modify: change double quotes in output to single quotes

--- cbfix.rb
+++ cbfix.rb
@@ -3,6 +3,6 @@
 cb_ver = IO.popen("knife cookbook list","r+")
 cb_ver.each_line do |line|
   /(?<name>[^\s]*)\s*(?<version>.*)/ =~ line
-  puts "\"#{name}\" => \"= #{version}\","
+  puts "\'#{name}\' => \'= #{version}\',"
 end
 cb_ver.close
